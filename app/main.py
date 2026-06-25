64a39b6215dda66fe45e36085ffff33b00c43e7f
Yordan <ayvazov.dani@gmail.com>
2026-06-25 17:03:46 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import zlib
+import hashlib
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -27,9 +28,36 @@ def main():
 
         content = content.split(b"\x00")[1].decode() # splitting by the bull bytes seprator, then deocding it to give us the text
 
-        print(content, end="")
+        print(content, end="") # by default print() adds a \n at the end
+    
+    elif command == 'hash-object':
+        filename = sys.argv[3]
 
+        with open(filename, 'rb') as f:
+            content = f.read()
         
+        header = f"blob {len(content)}\0" # constructing the blob header
+
+        blob_object = header.encode() + content # combining the header and the content
+
+        shahash_object = hashlib.sha1(blob_object) # constructs the hash object with the data
+
+        shahash = shahash_object.hexdigest() # extracts the result as a 40 char hash
+
+        compressed_blob = zlib.compress(blob_object) # compressing the blob
+
+        path = os.path.join('.git', "objects", shahash[:2], shahash[2:])
+
+        dir_path = os.path.join('.git', 'objects', shahash[:2]) # create the folder path only
+
+        if not os.path.exists(path): # check if the dir exists
+            os.mkdir(path) # create the full path
+
+        with open(path, 'wb') as f: # write the blob to the file
+            f.write(compressed_blob)
+
+        print(shahash)
+
 
     else:
         raise RuntimeError(f"Unknown command #{command}")
