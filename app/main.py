145cbcc9b507eb7dfa91e88bcbea78ff45ec8181
Yordan <ayvazov.dani@gmail.com>
2026-06-26 14:38:15 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,6 +3,63 @@ import os
 import zlib
 import hashlib
 
+
+
+def write_tree(path):
+
+    entries = []
+
+    for item in sorted(os.listdir(path)):
+
+        if item == ".git":
+            continue
+
+        full_path = os.path.join(path, item)
+
+        if os.path.isfile(full_path):
+
+            with open(item, 'rb') as f:
+                    content = f.read()
+
+                header = f"blob {len(content)}\0"
+
+                blob_object = header.encode() + content
+                
+                shahash_object = hashlib.sha1(blob_object)
+
+                shahash = shahash_object.digest()
+
+                entry = f"100644 {item}\x00".encode() + shahash
+
+
+                entries.append(entry)
+
+        elif os.path.isdir(full_path):
+            sub_sha = write_tree(full_path)
+            entry = f"040000 {item}\x00".encode() + bytes.fromhex(sub_sha)
+            entries.append(entry)
+
+        tree_data = b"".join(entries)
+
+        header = f'tree {len(tree_data)}\x00'.encode()
+        
+        full_tree = header + tree_data
+
+        h = hashlib.sha1(full_tree)
+        sha_hash = h.hexdigest()
+
+        path = os.path.join('.git', "objects", shahash[:2], shahash[2:])
+
+        dir_path = os.path.join('.git', 'objects', sha_hash[:2])
+
+        if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
+
+        with open(path, 'wb') as f:
+            f.write(zlib.compress(full_tree))
+
+        return sha_hash
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
@@ -74,11 +131,11 @@ def main():
         entries = []
 
         while content:
-            null_index = content.index(b"\x00")
+            null_index = content.index(b"\x00") # getting the null index which is equal to one byte
 
             mode_and_filename = content[:null_index]
 
-            filename = mode_and_filename.split(b" ", 1)[1].decode()
+            filename = mode_and_filename.split(b" ", 1)[1].decode() # getting the filename as text
 
             content = content[null_index + 1:] # move to the sha which is after the null_index
 
@@ -90,6 +147,11 @@ def main():
 
         for entry in entries:
             print(entry)
+
+
+    elif command = 'write-tree':
+        sha = write_tree(".")
+        print(sha)
 
 
     else:
